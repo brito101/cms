3cf610f35a0af6fb742f6849107680a64f794719
brito101 <rodrigobrito101@hotmail.com>
2022-06-27 13:16:35 -0300

    Install AdminLTE

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\HomeController;
+use App\Http\Controllers\Site\HomeController as SiteHomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [SiteHomeController::class, 'index']);
+
+Route::prefix('admin')->middleware('auth')->group(function () {
+    Route::get('/', [HomeController::class, 'index']);
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
